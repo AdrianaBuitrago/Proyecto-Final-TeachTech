82c3e328d89ed20e5d78a2fda97988e079d75e79
Adriana Buitrago <buitragoadr@gmail.com>
2022-07-23 14:03:36 +0200

    Botón iniciar juego, crear secuencia y mostrarla.

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// crear funcion numero random
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//   console.log(randomNumber(0,3));
+
+// función para conseguir posicion aleatoria del tablero
+function randomPosition() {
+    return randomNumber(0, 3);
+}
+
+// crear funcion para guardar secuencia con parametro entrada
+// donde una secuencia = array (numeros)
+
+// let sequenceArray =[]
+let level = 0;
+
+function createSequence(maxSequence) {
+    const sequence = []
+    for (let index = 0; index < maxSequence; index++) {
+        let aleatorio = randomPosition();
+        sequence.push(aleatorio)
+    }
+    return sequence
+}
+
+function increaseLevel() {
+    level = level + 1;
+}
+
+// Crear una funcion inicializar start_game, vinculado al button
+function startGame() {
+    increaseLevel();
+    const sequence = createSequence(level)
+    console.log(sequence)
+    // bucle for in de sequence
+
+    sequence.forEach((position, index) => {
+        console.log(position)
+
+        setTimeout(() => {
+            activePosition(position);
+        },(index +1)* 900);
+    });
+}
+// en cada vuelta llamar a setTimeout para poner los colores
+
+
+function deactivatePosition(position) {
+    let element0 = document.getElementById(position.toString());
+    element0.classList.remove("active");
+}
+
+function activePosition(position) {
+    let element0 = document.getElementById(position.toString());
+    element0.classList.add("active");
+    function deactivateCurrentPosition() {
+        deactivatePosition(position)
+    }
+
+    const myTimeout = setTimeout(deactivateCurrentPosition, 500);
+}
+
